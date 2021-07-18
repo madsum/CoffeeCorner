305e25250094dc088c3f73a783715f082d9ad773
Muhammed Masum Islam <madsum@gmail.com>
2021-07-18 20:58:16 +0200

    Common class MenuItem with builder pattern by TDD

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,46 @@
+package org.example;
+
+class Item {
+
+    private String name;
+    private float price;
+    private int quantity;
+    private String size;
+
+    public String getName() {
+        return name;
+    }
+
+    public Item(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+}
